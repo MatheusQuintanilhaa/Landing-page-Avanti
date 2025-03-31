d3d1971c2de5934888f651f97492aa16c56c1686
Matheus Quintanilha <matheussantos.quintanilha@gmail.com>
2025-03-31 16:50:42 -0300

    Header Finalizado

---
+++ assets/js/load-components.js
@@ -0,0 +1,16 @@
+// Função para carregar componentes
+function loadComponent(componentPath, targetElementId) {
+  fetch(componentPath)
+    .then((response) => response.text())
+    .then((html) => {
+      document.getElementById(targetElementId).innerHTML = html;
+    });
+}
+
+// Carregar componentes quando a página carregar
+window.onload = function () {
+  loadComponent("../../components/header.html", "header-container");
+  loadComponent("../../components/banner.html", "banner-container");
+  loadComponent("../../components/footer.html", "footer-container");
+  loadComponent("../../components/product-card.html", "product-container");
+};
