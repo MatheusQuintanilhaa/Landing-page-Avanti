3b962bfa6dd8ca7f23f3bbe02b73a19fed1a9b0f
Matheus Quintanilha <matheussantos.quintanilha@gmail.com>
2025-04-07 16:30:06 -0300

    feat: productFeatureSection foi criado e estilizado, e implementado a reutilizacao dos componentes featured-product-container e products-container

--- assets/js/load-components.js
+++ assets/js/load-components.js
@@ -1,18 +1,43 @@
-function loadComponent(componentPath, targetElementId) {
+function loadComponent(componentPath, targetSelector, callback) {
   fetch(componentPath)
     .then((response) => response.text())
     .then((html) => {
-      document.getElementById(targetElementId).innerHTML = html;
+      if (targetSelector.startsWith("#")) {
+        const element = document.getElementById(targetSelector.slice(1));
+        if (element) element.innerHTML = html;
+      } else if (targetSelector.startsWith(".")) {
+        document.querySelectorAll(targetSelector).forEach((el) => {
+          el.innerHTML = html;
+        });
+      } else {
+        console.warn(`Seletor inválido: ${targetSelector}`);
+      }
+
+      if (typeof callback === "function") {
+        callback();
+      }
+    })
+    .catch((error) => {
+      console.error(`Erro ao carregar o componente ${componentPath}:`, error);
     });
 }
 
 window.onload = function () {
-  loadComponent("../../components/header.html", "header-container");
-  loadComponent("../../components/banner.html", "banner-container");
-  loadComponent("../../components/footer.html", "footer-container");
-  // Call loadComponent for your new featured product component
+  loadComponent("../../components/header.html", "#header-container");
+  loadComponent("../../components/banner.html", "#banner-container");
+  loadComponent("../../components/footer.html", "#footer-container");
   loadComponent(
     "../../components/featured-product.html",
-    "featured-product-container"
+    ".featured-product-container"
+  );
+  loadComponent(
+    "../../components/productFeatureSection.html",
+    ".product-feature-section-container"
+  );
+
+  loadComponent(
+    "../../components/productsCarousel.html",
+    ".products-container-wrapper",
+    initializeSwiper
   );
 };
