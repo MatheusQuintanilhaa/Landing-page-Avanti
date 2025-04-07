556c80ed4bed0b42248c89ee7dbaa2b27fb9275e
Matheus Quintanilha <matheussantos.quintanilha@gmail.com>
2025-04-07 11:00:37 -0300

    feat: feature-product foi adicionado e estilizado

--- assets/js/load-components.js
+++ assets/js/load-components.js
@@ -10,4 +10,9 @@ window.onload = function () {
   loadComponent("../../components/header.html", "header-container");
   loadComponent("../../components/banner.html", "banner-container");
   loadComponent("../../components/footer.html", "footer-container");
+  // Call loadComponent for your new featured product component
+  loadComponent(
+    "../../components/featured-product.html",
+    "featured-product-container"
+  );
 };
